35349f49cb80b82363edd878155c09853d7617a0
Mark Sandora <marksandora@trueearthhealth.com>
2020-06-10 03:16:35 -0400

    end of section 15

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -5,6 +5,13 @@ import 'firebase/auth';
 import 'firebase/storage';
 
 const firebaseConfig = {
+  // apiKey: "AIzaSyAyCj6L0iNt7kJnxVtqrq3Y5MZ39CFiy9A",
+  // authDomain: "socialfamily-d58c8.firebaseapp.com",
+  // databaseURL: "https://socialfamily-d58c8.firebaseio.com",
+  // projectId: "socialfamily-d58c8",
+  // storageBucket: "socialfamily-d58c8.appspot.com",
+  // messagingSenderId: "581383764882"
+  
   apiKey: 'AIzaSyCvhJgcBOCFw7HcSGXU1f6B3Ez31J7oSbE',
   authDomain: 'socialfamilyapp.firebaseapp.com',
   databaseURL: 'https://socialfamilyapp.firebaseio.com',
--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,19 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+import { withFirebase } from 'react-redux-firebase';
 import { Menu, Container, Button } from 'semantic-ui-react';
 import { NavLink, Link, withRouter } from 'react-router-dom';
 import SignedOutMenu from '../Menus/SignedOutMenu';
 import SignedInMenu from '../Menus/SignedInMenu';
 import { openModal } from '../../modals/modalActions';
-import { logout } from '../../auth/authActions';
 
 const actions = {
   openModal,
-  logout,
 };
 
 const mapState = (state) => ({
-  auth: state.auth,
+  auth: state.firebase.auth,
+  profile: state.firebase.profile
 });
 
 class NavBar extends Component {
@@ -26,13 +26,13 @@ class NavBar extends Component {
   };
 
   handleSignOut = () => {
-    this.props.logout();
+    this.props.firebase.logout();
     this.props.history.push('/');
   };
 
   render() {
-    const { auth } = this.props;
-    const authenticated = auth.authenticated;
+    const { auth, profile } = this.props;
+    const authenticated = auth.isLoaded && !auth.isEmpty;
     return (
       <Menu inverted fixed='top'>
         <Container>
@@ -57,10 +57,7 @@ class NavBar extends Component {
             </Fragment>
           )}
           {authenticated ? (
-            <SignedInMenu
-              signOut={this.handleSignOut}
-              currentUser={auth.currentUser}
-            />
+            <SignedInMenu signOut={this.handleSignOut} profile={profile} />
           ) : (
             <SignedOutMenu
               signIn={this.handleSignIn}
@@ -73,4 +70,4 @@ class NavBar extends Component {
   }
 }
 
-export default withRouter(connect(mapState, actions)(NavBar));
+export default withRouter(withFirebase(connect(mapState, actions)(NavBar)));
